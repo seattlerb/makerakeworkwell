7c13fe20fe44fe0626f3edc3c585b7a60386c88f
Ryan Davis <ryand@zenspider.com>
2013-05-22 21:55:14 -0800

    deleted [git-p4: depot-paths = "//src/makerakeworkwell/dev/": change = 8593]

--- test/test_makerakeworkwell.rb
+++
@@ -1,8 +0,0 @@
-require "minitest/autorun"
-require "make/rake/work/well"
-
-class TestMakerakeworkwell < MiniTest::Unit::TestCase
-  def test_sanity
-    flunk "write tests or I will kneecap you"
-  end
-end
